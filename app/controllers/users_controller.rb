883cd1468d0d44a6ac0e01b725840856607153c3
shojikanamori <iru0515@gmail.com>
2016-06-27 07:16:54 +0000

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :set_params, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -18,9 +19,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = " Updated your Plofile "
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   
   def user_params
-    params.require(:user).permit(:name , :email , :password , :password_confirmation)
+    params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
+  end
+  
+  def set_params
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_path if !current_user?(@user)
   end
 end
